3d727f7424450df7cee30df43a46b7ed6005c255
jrbella <jbella@icstars.org>
2019-07-13 00:37:01 -0500

    added many changes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 
 //defining navbar code
 let navbarTemplate = `
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="">So You Wanna Buy Some Stuff</a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="">Deals</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Location</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Departments</a>
-            </li>
-            <li class="nav-item">
-                    <a class="nav-link" href="myCart.html">My Cart </a>
-                </li>
-        </ul>
-    </nav>    
+<nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+  <a class="navbar-brand" href="#">So you wanna buy some stuff?!!  </a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div id="navb" class="navbar-collapse collapse hide">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Deals</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Locations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Departments</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="myCart.html">My Cart</a>
+    </li>
+    </ul>
+
+    <ul class="nav navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fas fa-user"></span> Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signOn.html"><span class="fas fa-sign-in-alt"></span> Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>   
 `
 
 function newContent() {
